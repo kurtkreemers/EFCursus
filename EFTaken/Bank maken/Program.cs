0e16ab6e77e72743015f8915567601940cd5943e
Kurt Kreemers <kkreeme1@gmail.com>
2015-09-16 10:04:12 +0200

    taak1.2

--- EFTaken/Bank maken/Program.cs
+++ EFTaken/Bank maken/Program.cs
@@ -10,6 +10,29 @@ namespace Bank_maken
     {
         static void Main(string[] args)
         {
+            Program program = new Program();
+            foreach (var klant in program.FindAllRekeningen() )
+            {
+                Console.WriteLine(klant.Voornaam);
+                decimal totaal = 0;
+                foreach (var rekening in klant.Rekeningen)
+                {
+                    
+                    Console.WriteLine(rekening.RekeningNr + "=" + rekening.Saldo);
+                    totaal += rekening.Saldo;
+                }
+                Console.WriteLine("Totaal : {0}",totaal);
+                Console.WriteLine();
+            }
+            Console.ReadLine();
+        }
+        List<Klant> FindAllRekeningen()
+        {
+            using(var entities = new BankEntities())
+            {
+                return (from klant in entities.Klanten.Include("Rekeningen")
+                        select klant).ToList();                      
+            }
         }
     }
 }
